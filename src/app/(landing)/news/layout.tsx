e8bbcda52bf276a9934fa6678f83e27b8cffe8d3
Arsalan <arsalanbashir831@gmail.com>
2025-10-28 17:23:09 +0500

    pushed

--- src/app/(landing)/news/layout.tsx
+++ src/app/(landing)/news/layout.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
-import { Metadata } from 'next';
 import { NEWS_META_DATA } from '@/constants/meta-data';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 interface NewsLayoutProps {
   children: ReactNode;
 }
-
+// layout metadata
 export const metadata: Metadata = NEWS_META_DATA;
 
 export default function NewsLayout({ children }: NewsLayoutProps) {
